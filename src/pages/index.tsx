2b560589dbb994bfe1d07d28abff3a88217773ff
Bruno Dias <brunodias40@gmail.com>
2024-09-03 13:30:45 -0300

    feat: add Table component for displaying customer data in index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,24 @@
 import Layout from "@/components/Layout";
+import Table from "@/components/Table";
+import Cliente from "@/core/Cliente";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const clientes = [
+    new Cliente("Ana", 34, "1"),
+    new Cliente("Bia", 45, "2"),
+    new Cliente("Carlos", 23, "3"),
+    new Cliente("Pedro", 54, "4"),
+  ];
+
   return (
     <main
       className={`flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 to-blue-600 ${inter.className}`}
     >
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <Table clientes={clientes}></Table>
       </Layout>
     </main>
   );
